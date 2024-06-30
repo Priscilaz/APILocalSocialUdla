6721ba27aa8f48bfd412b2421b856303a7001688
DanielaMoraDevJourney <danielamoradev@outlook.com>
2024-06-30 11:23:28 -0500

    Creacción de CommentRepository

--- UdlaBlog.WebAPI.Local/Infrastructure/Repositories/CommentRepository.cs
+++ UdlaBlog.WebAPI.Local/Infrastructure/Repositories/CommentRepository.cs
@@ -1,6 +1,52 @@
-﻿namespace UdlaBlog.WebAPI.Local.Infrastructure.Repositories
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using UdlaBlog.WebAPI.Local.Domain.Interfaces;
+using UdlaBlog.WebAPI.Local.Domain.Models;
+using UdlaBlog.WebAPI.Local.Infrastructure.Data.Context;
+
+namespace UdlaBlog.WebAPI.Local.Infrastructure.Data.Repositories
 {
-    public class CommentRepository
+    public class CommentRepository : ICommentRepository
     {
+        private readonly ApplicationDbContext _context;
+
+        public CommentRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Comment> GetByIdAsync(Guid id)
+        {
+            return await _context.Comments.FindAsync(id);
+        }
+
+        public async Task<IEnumerable<Comment>> GetAllByBlogPostIdAsync(Guid blogPostId)
+        {
+            return await _context.Comments.Where(c => c.BlogPostId == blogPostId).ToListAsync();
+        }
+
+        public async Task AddAsync(Comment comment)
+        {
+            await _context.Comments.AddAsync(comment);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(Comment comment)
+        {
+            _context.Comments.Update(comment);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(Guid id)
+        {
+            var comment = await _context.Comments.FindAsync(id);
+            if (comment != null)
+            {
+                _context.Comments.Remove(comment);
+                await _context.SaveChangesAsync();
+            }
+        }
     }
 }
