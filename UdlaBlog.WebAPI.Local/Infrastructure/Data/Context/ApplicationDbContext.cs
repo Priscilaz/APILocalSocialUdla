b67213f495b1c5cdef63d3689d94eafdb87ad620
DanielaMoraDevJourney <danielamoradev@outlook.com>
2024-06-30 11:22:58 -0500

    ReContrucción del DBContext

--- UdlaBlog.WebAPI.Local/Infrastructure/Data/Context/ApplicationDbContext.cs
+++ UdlaBlog.WebAPI.Local/Infrastructure/Data/Context/ApplicationDbContext.cs
@@ -1,6 +1,34 @@
-﻿namespace UdlaBlog.WebAPI.Local.Infrastructure.Data.Context
+﻿using Microsoft.EntityFrameworkCore;
+using UdlaBlog.WebAPI.Local.Domain.Models;
+
+namespace UdlaBlog.WebAPI.Local.Infrastructure.Data.Context
 {
-    public class ApplicationDbContext
+    public class ApplicationDbContext : DbContext
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<BlogPost> BlogPosts { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<BlogPost>()
+                .HasMany(b => b.Tags)
+                .WithMany(t => t.BlogPosts);
+
+            modelBuilder.Entity<BlogPost>()
+                .HasMany(b => b.Comments)
+                .WithOne(c => c.BlogPost)
+                .HasForeignKey(c => c.BlogPostId);
+
+            modelBuilder.Entity<User>().HasKey(u => u.Username);
+        }
     }
 }
