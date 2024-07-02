df8741be1ab0ed41a41d69ef09d1069da1225726
DanielaMoraDevJourney <danielamoradev@outlook.com>
2024-07-02 07:10:04 -0500

    Agregando sección NODO Y FICA, lógica base

--- UdlaBlog.WebAPI.Local/Dominio/Models/BlogPost.cs
+++ UdlaBlog.WebAPI.Local/Dominio/Models/BlogPost.cs
@@ -1,43 +1,42 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
+using UdlaBlog.WebAPI.Local.Domain.Models;
 
-namespace UdlaBlog.WebAPI.Local.Domain.Models
+public class BlogPost
 {
-    public class BlogPost
-    {
-        public Guid Id { get; set; }
+    public Guid Id { get; set; }
 
-        [Required]
-        public string Encabezado { get; set; }
+    [Required]
+    public string Encabezado { get; set; }
 
-        [Required]
-        public string TituloPagina { get; set; }
+    [Required]
+    public string TituloPagina { get; set; }
 
-        [Required]
-        public string Contenido { get; set; }
+    [Required]
+    public string Contenido { get; set; }
 
-        [Required]
-        public string DescripcionCorta { get; set; }
+    [Required]
+    public string DescripcionCorta { get; set; }
 
-        public string UrlImagenDestacada { get; set; }
+    public string UrlImagenDestacada { get; set; }
 
-        public string ManejadorUrl { get; set; }
+    public string ManejadorUrl { get; set; }
 
-        [Required]
-        public DateTime FechaPublicacion { get; set; }
+    [Required]
+    public DateTime FechaPublicacion { get; set; }
 
-        [Required]
-        public string Autor { get; set; }
+    [Required]
+    public string Autor { get; set; }
 
-        [Required]
-        public bool Visible { get; set; }
+    [Required]
+    public bool Visible { get; set; }
 
-        [Required]
-        public ICollection<Tag> Tags { get; set; } = new List<Tag>();
+    [Required]
+    public ICollection<Tag> Tags { get; set; } = new List<Tag>();
 
-        public ICollection<Comment> Comments { get; set; } = new List<Comment>(); 
+    public ICollection<Comment> Comments { get; set; } = new List<Comment>();
 
-        public string RutaImagen { get; set; } 
-    }
+    public string RutaImagen { get; set; }
+
+    [Required]
+    public string Section { get; set; }
 }
